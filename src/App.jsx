ff08c923825134790efb8a125a0969acd2b414b9
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-09-09 15:38:11 -0400

    dice initialized through state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import { useState } from 'react'
+import Dice from './Dice'
 import './App.css'
 
+
+
+
 function App() {
+  const [dice, setDice] = useState(newDice())
+
+  function newDice(){
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(newDice())
+  }
+  
+  const diceElements = dice.map(die => <Dice value={die} />)
+
   return (
       <main>
+        <div className="dice_container">
+          {diceElements}
+        </div>
+        <button className="roll-dice" onClick={rollDice}>Roll</button>
       </main>
   )
 }
